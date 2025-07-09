60eeb7d3d6fa19048d252e08c46fe207b52131e2
mariosantos2003-dot <mariosantosbl2003@gmail.com>
2025-07-09 12:30:30 +0200

    Inicio poryecto

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gradient-to-r from-amber-300 to-pink-500">
+      <h1 className="text-5xl font-extrabold text-white shadow-lg">
+        ¡Marc Store con Tailwind está lista! 💍
+      </h1>
+    </main>
+  );
+}
