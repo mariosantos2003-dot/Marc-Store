ed32b0bf9355cb4042c81539999b7c2e6309b954
mariosantos2003-dot <mariosantosbl2003@gmail.com>
2025-07-11 21:59:29 +0200

    Funciona modelo 3d

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,38 +1,26 @@
 // src/components/Logo3D.jsx
-import { Canvas } from '@react-three/fiber';
-import { Text3D, Center, OrbitControls, Environment } from '@react-three/drei';
-import { Suspense } from 'react';
-import font from '../assets/fonts/font.json';
+import { Canvas, useLoader, useThree } from "@react-three/fiber";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+import { TextureLoader } from "three";
 
-export default function Logo() {
-  return (
-    <div className>
-      <Canvas camera={{ position: [0, 0, 15], fov: 50 }}>
-        <ambientLight intensity={0.6} />
-        <directionalLight position={[10, 10, 5]} intensity={1} />
-        <Suspense fallback={null}>
-          <Environment preset="sunset" />
-          <Center>
-            <Text3D
-              font="sans-serif"
-              size={3}
-              height={1}
-              curveSegments={16}
-              bevelEnabled
-              bevelThickness={0.15}
-              bevelSize={0.1}
-            >
-              PANICO {/* Cambia esto por el nombre del proyecto */}
-              <meshStandardMaterial
-                metalness={1}
-                roughness={0.1}
-                
-              />
-            </Text3D>
-          </Center>
-        </Suspense>
-        <OrbitControls enableZoom={false} />
-      </Canvas>
-    </div>
-  );
+
+function LogoModel(){
+  const {scene} = useGLTF("public/assets/logo.glb");
+  return <primitive object={scene}/>
+}
+function Background(){
+  const texture = useLoader(TextureLoader, "public/assets/background.jpg");
+  const {scene} = useThree();
+  scene.background = texture;
+}
+
+export default function Logo(){
+  return(
+    <Canvas style={{height: "100%", width: "100%", backgroundImage: "url(./assets/background.jpg)"}}>
+      <ambientLight />
+      <directionalLight position={[2,2,2]} />
+      <LogoModel />
+      <OrbitControls enableZoom={false}/>
+    </Canvas>
+  )
 }
