7c16f0c438360bd0c6dd964a8a1d03fd1f07551b
mariosantos2003-dot <mariosantosbl2003@gmail.com>
2025-07-16 20:36:12 +0200

    visor 3d hecho

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -1,36 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
 import { products } from "../../utils/products";
-import "./ProductDetail.css"; 
-import gsap from "gsap";// Asegúrate de tener este archivo CSS
+import "./ProductDetail.css";
+import ModelViewer from "../ModelViewer/ModelViewer";
 
 function ProductDetail() {
   const { id } = useParams();
   const product = products.find((p) => p.id.toString() === id);
+  const [showModel, setShowModel] = useState(false);
 
-  if (!product) {
-    return <div>Product not found</div>;
-  }
-  useEffect(() => {
-    gsap.from(".product-detail", {
-      opacity: 1,
-      y: 50,
-      duration: 1,
-      ease: "power2.out",
-    });
-  }, []);
-  
+  if (!product) return <div>Product not found</div>;
+
   return (
     <div className="product-detail">
       <div className="product-image-container">
         <img src={product.img} alt={product.nombre} />
-        <button className="view-button"> Ver en 3d </button>
+        <button
+          className="view-button"
+          onClick={() => setShowModel(!showModel)}
+        >
+          {showModel ? "Ocultar 3D" : "Ver en 3D"}
+        </button>
+        {showModel && <ModelViewer modelPath="/assets/Hand-Final.glb" onClose={() => setShowModel(false)} />}
       </div>
 
       <div className="product-info">
         <h1 className="product-title">{product.nombre}</h1>
-
         <p className="product-description">{product.descripcion}</p>
         <p className="product-price">${product.precio}</p>
       </div>
